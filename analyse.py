1a09f025fc88da5da3cd430df51f56dba07d5a13
Semjon <semjonglushkov@yandex.ru>
2018-05-09 07:30:33 +0300

    3D representation is done, but it is very slow

--- analyse.py
+++ analyse.py
@@ -1,12 +1,13 @@
 import sys
 import numpy as np
-import matplotlib
-import types
-from Bio.Seq import Seq
 from Bio import SeqIO
 from Bio.Alphabet import IUPAC
 from matplotlib import pyplot as plt
+import matplotlib.cm
+import matplotlib.colorbar
+import matplotlib.colors
 from mpl_toolkits.mplot3d import axes3d
+
 
 class AnalysisRes:
     def __init__(self, tensor, acid_dict):
@@ -64,8 +65,66 @@ class AnalysisRes:
         plt.show()
 
     def _plot_heat3d(self):
-        pass
+        fig = plt.figure(figsize=(8, 6))
+        ax = fig.gca(projection='3d')
+        labels = [""] * len(self.acid_dict)
+        for key in self.acid_dict:
+            labels[self.acid_dict[key]] = key
+        ax.set_xticks(np.arange(len(self.acid_dict)))
+        ax.set_yticks(np.arange(len(self.acid_dict)))
+        ax.set_zticks(np.arange(len(self.acid_dict)))
+        ax.set_xticklabels(labels)
+        ax.set_yticklabels(labels)
+        ax.set_zticklabels(labels)
+        ax.set_xlabel("condition 2 amino acids", fontdict={'size':15})
+        ax.set_ylabel("condition 1 amino acids", fontdict={'size':15})
+        ax.set_zlabel("actual amino acids", fontdict={'size':15})
+        colmap = matplotlib.cm.ScalarMappable(cmap="coolwarm")
+        colmap.set_array([0, 1])
+        # X, Y, Z = [], [], []
+        # for i in range(len(self.acid_dict)):
+        #         for j in range(len(self.acid_dict)):
+        #             for k in range(len(self.acid_dict)):
+        #                 X = X + [i] * len(self.acid_dict)
+        #                 Y = Y + [j] * len(self.acid_dict)
+        #                 Z = Z + [k] * len(self.acid_dict)
+        # X = np.array(X)
+        # Y = np.array(Y)
+        # Z = np.array(Z)
+        # D = [self.tensor[X[i]][Y[i]][Z[i]] for i in range(len(X))]
+        # print(X.shape)
+        # ax.scatter(X, Y, Z, marker='s', s=140,
+        #            c=D , cmap='coolwarm', vmin=0, vmax=1, alpha=0.05);
+        cb = fig.colorbar(colmap)
+        norm = matplotlib.colors.Normalize(vmin=0, vmax=1)
+        colors = lambda i, j, k: matplotlib.cm.ScalarMappable(norm=norm, cmap="coolwarm").to_rgba(self.tensor[i, j, k])
+        for i in range(len(self.acid_dict)):
+                for j in range(len(self.acid_dict)):
+                    for k in range(len(self.acid_dict)):
+                        X, Y, Z = self._get_cube(k, j, i, 1)
+                        ax.plot_surface(X, Y, Z, color=colors(i, j, k), rstride=1, cstride=1, alpha=0.03)
+        plt.show()
 
+    def _get_cube(self, x, y, z, edge):
+        X = np.array([[0., 1., 1., 0., 0.],
+                      [0., 1., 1., 0., 0.],
+                      [0., 1., 1., 0., 0.],
+                      [0., 1., 1., 0., 0.]])
+        Y = np.array([[0., 0., 1., 1., 0.],
+                      [0., 0., 1., 1., 0.],
+                      [0., 0., 0., 0., 0.],
+                      [1., 1., 1., 1., 1.]])
+        Z = np.array([[0., 0., 0., 0., 0.],
+                      [1., 1., 1., 1., 1.],
+                      [0., 0., 1., 1., 0.],
+                      [0., 0., 1., 1., 0.]])
+        X *= edge
+        Y *= edge
+        Z *= edge
+        X = X + np.full((4, 5), x - edge / 2)
+        Y = Y + np.full((4, 5), y - edge / 2)
+        Z = Z + np.full((4, 5), z - edge / 2)
+        return X, Y, Z
 
 def analyse(alignment, *args):
     acids = IUPAC.protein.letters + "U"
@@ -120,7 +179,7 @@ if __name__ == "__main__":
     fin = sys.argv[1]
     print("Input file: {0}".format(fin))
     sequence_record = SeqIO.parse(fin, 'fasta')
-    res = analyse(sequence_record, 160, 170)
+    res = analyse(sequence_record, 160, 170, 180)
     sequence_record = SeqIO.parse(fin, 'fasta')
     for rec in sequence_record:
         print(rec.seq)
