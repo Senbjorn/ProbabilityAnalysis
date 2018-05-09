bf981751a6b9e931377d65a27cd44ad8f2892007
Semjon <semjonglushkov@yandex.ru>
2018-05-09 03:19:41 +0300

    New alignment added. Work on 3D representation is in progerss

--- analyse.py
+++ analyse.py
@@ -3,40 +3,88 @@ import numpy as np
 from Bio.Seq import Seq
 from Bio import SeqIO
 from Bio.Alphabet import IUPAC
+from matplotlib import pyplot as plt
 
+class AnalysisRes:
+    def __init__(self, tensor, acid_dict):
+        if not isinstance(tensor, np.ndarray):
+            raise TypeError('parameter tensor must be an instance of numpy.ndarray.')
+        self.tensor = tensor
+        self.dim = len(self.tensor.shape)
+        self.acid_dict = acid_dict
+
+    def plot_heat(self):
+        if self.dim == 1:
+            self._plot_heat1d()
+        elif self.dim == 2:
+            self._plot_heat2d()
+        elif self.dim == 3:
+            self._plot_heat3d()
+        else:
+            raise TypeError("data must be not more then 3-dimensional.")
+
+    def _plot_heat1d(self):
+        fig, ax = plt.subplots(figsize=(8, 6))
+        x = np.arange(len(self.acid_dict))
+        labels = [""] * len(self.acid_dict)
+        for key in self.acid_dict:
+            labels[self.acid_dict[key]] = key
+        ax.bar(x, self.tensor, tick_label=labels, color='g')
+        ax.set_xlabel("amino acids", fontdict={'size':17})
+        ax.set_ylabel("frequency", fontdict={'size':17})
+        ax.set_title("1D result representation", fontdict={'size':20})
+        plt.show()
+
+    def _plot_heat2d(self):
+        fig, ax = plt.subplots(figsize=(8, 6))
+        ax.imshow(self.tensor, vmin=0, vmax=1, cmap='Blues', origin=[0, len(self.acid_dict)])
+        ax.set_xticks(np.arange(len(self.acid_dict)))
+        ax.set_yticks(np.arange(len(self.acid_dict)))
+        labels = [""] * len(self.acid_dict)
+        for key in self.acid_dict:
+            labels[self.acid_dict[key]] = key
+        ax.set_xticklabels(labels)
+        ax.set_yticklabels(labels)
+        ax.set_xlabel("actual amino acids", fontdict={'size':17})
+        ax.set_ylabel("condition amino acids", fontdict={'size':17})
+        ax.set_title("2D data representation", fontdict={'size':20})
+        plt.show()
+
+    def _plot_heat3d(self):
+        pass
 
 def analyse(alignment, *args):
     acids = IUPAC.protein.letters + "U"
     acid_dict = {acids[i]: i for i in range(len(acids))}
     nacid = len(acids)
     if len(args) == 1:
-        total = 0
         pos1 = args[0]
-        vector = [0] * nacid
+        total = 0
+        vector = np.zeros(nacid)
         for record in alignment:
             total += 1
             if record.seq[pos1] in acid_dict:
                 vector[acid_dict[record.seq[pos1]]] += 1;
-        return np.array(vector) / total
+        vector /= total
+        return AnalysisRes(vector, acid_dict)
     elif len(args) == 2:
-        pos1, pos2 = args[0], args[1]
-        total = [0] * nacid
-        matrix = [[0] * nacid for i in range(nacid)]
+        pos1, pos2 = args
+        total = np.zeros(nacid)
+        matrix = np.zeros((nacid, nacid))
         for record in alignment:
             if record.seq[pos1] in acid_dict and record.seq[pos2] in acid_dict:
                 ind1, ind2 = acid_dict[record.seq[pos1]], acid_dict[record.seq[pos2]]
                 total[ind2] += 1
                 matrix[ind1][ind2] += 1
-        matrix = np.array(matrix)
         for i in range(nacid):
             for j in range(nacid):
                 if total[j] != 0:
                     matrix[i][j] /= total[j]
-        return matrix
+        return AnalysisRes(matrix, acid_dict)
     elif len(args) == 3:
-        pos1, pos2, pos3 = args[0], args[1], args[2]
-        total = [[0] * nacid for i in range(nacid)]
-        tensor = [[[0] * nacid for j in range(nacid)] for i in range(nacid)]
+        pos1, pos2, pos3 = args
+        total = np.zeros((nacid, nacid))
+        tensor = np.zeros((nacid, nacid, nacid))
         for record in alignment:
             if record.seq[pos1] in acid_dict and record.seq[pos2] in acid_dict and record.seq[pos3] in acid_dict:
                 ind1, ind2, ind3 = acid_dict[record.seq[pos1]], acid_dict[record.seq[pos2]], acid_dict[record.seq[pos3]]
@@ -47,6 +95,7 @@ def analyse(alignment, *args):
                 for k in range(nacid):
                     if total[j][k] != 0:
                         tensor[i][j][k] /= total[j][k]
+        return AnalysisRes(tensor, acid_dict)
     else:
         raise TypeError('analyse() takes from 2 to 4 arguments but {0:d} were given.'.format(len(args) + 1))
 
@@ -55,5 +104,8 @@ if __name__ == "__main__":
     fin = sys.argv[1]
     print("Input file: {0}".format(fin))
     sequence_record = SeqIO.parse(fin, 'fasta')
-    print(*analyse(sequence_record, 3, 8), sep="\n")
+    # for rec in sequence_record:
+    #     print(rec.seq)
+    res = analyse(sequence_record, 160, 170)
+    res.plot_heat()
     print("Done!")
